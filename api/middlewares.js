25b885279e2723f3288228301f5a9aa9e13217bd
kazuma1989 <kazuma.ebina@quipper.com>
2021-04-24 18:26:22 +0900

    feat: ファイルをアップロードできるように

--- api/middlewares.js
+++ api/middlewares.js
@@ -1,3 +1,6 @@
+const fs = require("fs")
+const path = require("path")
+
 /**
  * [JSON Server](https://github.com/typicode/json-server) のミドルウェア。
  *
@@ -7,7 +10,11 @@
  * @typedef {(req: Request, res: Response, next: NextFunction) => void} Middleware
  */
 
-module.exports = [latency(1000, 100), totalComments()]
+module.exports = [
+  latency(1000, 100),
+  totalComments(),
+  storage("/_storage", path.resolve(process.cwd(), "_storage")),
+]
 
 /**
  * ランダムなレイテンシーを再現する。
@@ -56,6 +63,55 @@ function totalComments() {
             .find({ id: comment.postId })
             .update("totalComments", (v) => v - 1)
             .write()
+        }
+        break
+      }
+    }
+
+    next()
+  }
+}
+
+const mimeTypes = {
+  ".png": "image/png",
+  ".jpg": "image/jpg",
+  ".jpeg": "image/jpg",
+  ".gif": "image/gif",
+}
+
+/**
+ * ファイルをアップロードする。
+ *
+ * @param {string} url
+ * @param {string} dir
+ * @return {Middleware}
+ */
+function storage(url, dir) {
+  return async (req, res, next) => {
+    switch (req.method) {
+      case "POST": {
+        if (req.path === url) {
+          const [extname] =
+            Object.entries(mimeTypes).find(([, mimeType]) =>
+              req.headers["content-type"].startsWith(mimeType)
+            ) || []
+          if (!extname) break
+
+          const filename = `${Date.now()}${extname}`
+
+          await new Promise((resolve, reject) => {
+            req
+              .pipe(fs.createWriteStream(path.join(dir, filename)))
+              .once("finish", resolve)
+              .once("error", reject)
+          })
+
+          res.status(201).json({
+            downloadURL: `${req.protocol}://${req.get(
+              "host"
+            )}${url}/${filename}`,
+          })
+          return
         }
         break
       }
