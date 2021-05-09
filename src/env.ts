f1bbee28af22f9da1d205979d76057ece2a68635
kazuma1989 <kazuma.ebina@quipper.com>
2021-05-09 10:37:35 +0900

    refactor: env.ts をシンプルな書き方にした

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,6 @@
-/**
- * ビルド時に設定される環境変数をまとめる。
- *
- * 環境変数の扱いはビルドツールによって異なるため、その差異をこのモジュールで吸収する。
- */
+// ビルド時に設定される環境変数をまとめる。
+// 環境変数の扱いはビルドツールによって異なるため、その差異をこのモジュールで吸収する。
+// .env.development, .env.production に値をセットする必要あり。
 
-export { ENV_MODE, ENV_API_ENDPOINT }
+export const ENV_MODE = import.meta.env.MODE
+export const ENV_API_ENDPOINT = import.meta.env.API_ENDPOINT
